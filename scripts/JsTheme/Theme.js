cc60860ca97cc121e3da161c3971464b612074ff
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-16 23:56:36 -0300

    update theme.js

--- scripts/JsTheme/Theme.js
+++ scripts/JsTheme/Theme.js
@@ -2,14 +2,17 @@ const mode = document.getElementById('mode_icon');
 
 mode.addEventListener('click', () => {
     const form = document.querySelector('.darkmode');
+    const form2 = document.querySelector('.mode');
 
-    console.log(form)
+
+    console.log
 
     if(mode.classList.contains('fa-moon')) {
         mode.classList.remove('fa-moon');
         mode.classList.add('fa-sun');
 
         form.classList.add('dark');
+        form2.classList.add('mode');
         return ;
     }
     
@@ -17,22 +20,6 @@ mode.addEventListener('click', () => {
     mode.classList.add('fa-moon');
 
     form.classList.remove('dark');
+    form2.classList.remove('dark');
 });
 
-mode.addEventListener('click', () => {
-    const form = document.querySelector('.mode');
-
-
-    if(mode.classList.contains('fa-moon')) {
-        mode.classList.remove('fa-moon');
-        mode.classList.add('fa-sun');
-
-        form.classList.add('mode');
-        return ;
-    }
-    
-    mode.classList.remove('fa-sun');
-    mode.classList.add('fa-moon');
-
-    form.classList.remove('mode');
-});

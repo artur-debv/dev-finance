8eba6a4e316b3fc16c65a59d5f1dee3138c56dbe
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-21 10:38:45 -0300

    update Form.js

--- scripts/Form/Form.js
+++ scripts/Form/Form.js
@@ -45,28 +45,5 @@ function myMenuFunction() {
                 form.classList.remove('dark');
                 form2.classList.remove('select');
             }
-        }
-    
-        function toggleTheme() {
-            const currentTheme = localStorage.getItem('theme') || 'light';
-            const newTheme = currentTheme === 'light' ? 'dark' : 'light';
-            localStorage.setItem('theme', newTheme);
-            setTheme(newTheme);
-        }
-    
-        function setThemeFromPreference() {
-            const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
-            const systemPreference = prefersDarkScheme.matches ? 'dark' : 'light';
-            if (!localStorage.getItem('theme')) {
-                localStorage.setItem('theme', systemPreference);
-            }
-            setTheme(localStorage.getItem('theme'));
-        }
-    
-        setThemeFromPreference();
-    
-        mode.addEventListener('click', () => {
-            toggleTheme();
-        });
     });
     

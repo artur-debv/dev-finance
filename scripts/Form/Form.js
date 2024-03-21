b9d44558d421bd1a076de57d8f271014e708030c
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-21 10:37:49 -0300

    update Form.js

--- scripts/Form/Form.js
+++ scripts/Form/Form.js
@@ -27,3 +27,46 @@ function myMenuFunction() {
         x.style.opacity = 0;
         y.style.opacity = 1;
     }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const mode = document.getElementById('mode_icon');
+        const form = document.querySelector('.darkmode');
+        const form2 = document.querySelector('.mode');
+    
+        function setTheme(theme) {
+            if (theme === 'dark') {
+                mode.classList.remove('fa-moon');
+                mode.classList.add('fa-sun');
+                form.classList.add('dark');
+                form2.classList.add('select');
+            } else {
+                mode.classList.remove('fa-sun');
+                mode.classList.add('fa-moon');
+                form.classList.remove('dark');
+                form2.classList.remove('select');
+            }
+        }
+    
+        function toggleTheme() {
+            const currentTheme = localStorage.getItem('theme') || 'light';
+            const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+            localStorage.setItem('theme', newTheme);
+            setTheme(newTheme);
+        }
+    
+        function setThemeFromPreference() {
+            const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+            const systemPreference = prefersDarkScheme.matches ? 'dark' : 'light';
+            if (!localStorage.getItem('theme')) {
+                localStorage.setItem('theme', systemPreference);
+            }
+            setTheme(localStorage.getItem('theme'));
+        }
+    
+        setThemeFromPreference();
+    
+        mode.addEventListener('click', () => {
+            toggleTheme();
+        });
+    });
+    

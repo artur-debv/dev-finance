5b77073c0b3cf7577369410c2e31d98f6ac51cf6
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-24 19:59:26 -0300

    update form.js

--- scripts/Form/Form.js
+++ scripts/Form/Form.js
@@ -1,91 +1,87 @@
 // alternar entre fazer login e cadastrar
 
 function myMenuFunction() {
-    var i = document.getElementById("navMenu");
-    if(i.className === "nav-menu") {
-        i.className += " responsive";
+  var i = document.getElementById("navMenu");
+  if (i.className === "nav-menu") {
+    i.className += " responsive";
+  } else {
+    i.className = "nav-menu";
+  }
+}
+
+var a = document.getElementById("loginBtn");
+var b = document.getElementById("registerBtn");
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+function login() {
+  x.style.left = "4px";
+  y.style.right = "-520px";
+  a.className += " white-btn";
+  b.className = "btn";
+  x.style.opacity = 1;
+  y.style.opacity = 0;
+}
+function register() {
+  x.style.left = "-510px";
+  y.style.right = "5px";
+  a.className = "btn";
+  b.className += " white-btn";
+  x.style.opacity = 0;
+  y.style.opacity = 1;
+}
+
+// função para darkmode/lightmode
+document.addEventListener("DOMContentLoaded", () => {
+  const mode = document.getElementById("mode_icon");
+  const form = document.querySelector(".modes");
+
+  function setTheme(theme) {
+    if (theme === "dark") {
+      mode.classList.remove("fa-moon");
+      mode.classList.add("fa-sun");
+      form.classList.add("dark");
     } else {
-        i.className = "nav-menu";
+      mode.classList.remove("fa-sun");
+      mode.classList.add("fa-moon");
+      form.classList.remove("dark");
     }
-   }
+  }
 
-    var a = document.getElementById("loginBtn");
-    var b = document.getElementById("registerBtn");
-    var x = document.getElementById("login");
-    var y = document.getElementById("register");
-    function login() {
-        x.style.left = "4px";
-        y.style.right = "-520px";
-        a.className += " white-btn";
-        b.className = "btn";
-        x.style.opacity = 1;
-        y.style.opacity = 0;
+  function toggleTheme() {
+    const currentTheme = localStorage.getItem("theme") || "light";
+    const newTheme = currentTheme === "light" ? "dark" : "light";
+    localStorage.setItem("theme", newTheme);
+    setTheme(newTheme);
+  }
+
+  function setThemeFromPreference() {
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+    const systemPreference = prefersDarkScheme.matches ? "dark" : "light";
+    if (!localStorage.getItem("theme")) {
+      localStorage.setItem("theme", systemPreference);
     }
-    function register() {
-        x.style.left = "-510px";
-        y.style.right = "5px";
-        a.className = "btn";
-        b.className += " white-btn";
-        x.style.opacity = 0;
-        y.style.opacity = 1;
-    }
+    setTheme(localStorage.getItem("theme"));
+  }
 
-    // função para darkmode/lightmode
-    document.addEventListener('DOMContentLoaded', () => {
-        const mode = document.getElementById('mode_icon');
-        const form = document.querySelector('.modes');
-     
-    
-        function setTheme(theme) {
-            if (theme === 'dark') {
-                mode.classList.remove('fa-moon');
-                mode.classList.add('fa-sun');
-                form.classList.add('dark');
-            } else {
-                mode.classList.remove('fa-sun');
-                mode.classList.add('fa-moon');
-                form.classList.remove('dark');
-            }
-        }
-    
-        function toggleTheme() {
-            const currentTheme = localStorage.getItem('theme') || 'light';
-            const newTheme = currentTheme === 'light' ? 'dark' : 'light';
-            localStorage.setItem('theme', newTheme);
-            setTheme(newTheme);
-        }
-    
-        function setThemeFromPreference() {
-            const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
-            const systemPreference = prefersDarkScheme.matches ? 'dark' : 'light';
-            if (!localStorage.getItem('theme')) {
-                localStorage.setItem('theme', systemPreference);
-            }
-            setTheme(localStorage.getItem('theme'));
-        }
-    
-        setThemeFromPreference();
-    
-        mode.addEventListener('click', () => {
-            toggleTheme();
-        });
-    });
+  setThemeFromPreference();
 
-   
+  mode.addEventListener("click", () => {
+    toggleTheme();
+  });
+});
 
-    function handleCredentialResponse(response) {
-        console.log("Encoded JWT ID token: " + response.credential);
-      }
-      window.onload = function () {
-        google.accounts.id.initialize({
-          client_id: "YOUR_GOOGLE_CLIENT_ID",
-          callback: handleCredentialResponse
-        });
-        google.accounts.id.renderButton(
-          document.getElementById("buttonDiv"),
-          { theme: "outline", size: "large" }  // customization attributes
-        );
-        google.accounts.id.prompt(); // also display the One Tap dialog
-      }
-    
-    
+function handleCredentialResponse(response) {
+  console.log("Encoded JWT ID token: " + response.credential);
+}
+window.onload = function () {
+  google.accounts.id.initialize({
+    client_id:
+      "939125828914-u6tbs2k30r4tn6fr17k0erjb6j39l69d.apps.googleusercontent.com",
+    callback: handleCredentialResponse,
+  });
+  google.accounts.id.renderButton(
+    document.getElementById("buttonDiv"),
+    { theme: "outline", size: "large" } // customization attributes
+  );
+  google.accounts.id.prompt(); // also display the One Tap dialog
+};

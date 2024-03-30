3f15e64806542b9d6b1ba70bda32773135f92441
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-30 01:26:08 -0300

    update index.js

--- scripts/Index/index.js
+++ scripts/Index/index.js
@@ -1,16 +1,16 @@
 // Função para obter parâmetros de query string da URL
-function getParameterByName(email, url) {
+function getParameterByName(name, url) {
     if (!url) url = window.location.href;
-    email= email.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + email + "(=([^&#]*)|&|#|$)"),
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
         results = regex.exec(url);
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-const infos = document.querySelector('.item-description').innerHTML =   email
-const avatar = document.getElementById('user_avatar')
+const infos = document.querySelector('.item-description').innerHTML =   name
+const avatar = document.getElementById('user_avatar').src = picture
 
 
 // Obter os valores dos parâmetros de query string
@@ -18,9 +18,7 @@ var name = getParameterByName('name');
 var email = getParameterByName('email');
 var picture = getParameterByName('picture');
 
-
 // Usar os valores obtidos conforme necessário
 
 console.log("Email: " + email);
-
-
+console.log("Foto do Perfil: " + picture);

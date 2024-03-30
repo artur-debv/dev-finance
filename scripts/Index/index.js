7f00be507d73a3d68f6d11fd719bae6aeccb1df3
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-30 01:15:27 -0300

    update index.js

--- scripts/Index/index.js
+++ scripts/Index/index.js
@@ -10,7 +10,7 @@ function getParameterByName(name, url) {
 }
 
 const infos = document.querySelector('.item-description').innerHTML =   name
-const avatar = document.getElementById('user_avatar').innerHTML = `${}`
+const avatar = document.getElementById('user_avatar').innerHTML = `${email}`
 
 
 // Obter os valores dos parâmetros de query string

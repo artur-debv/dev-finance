91239333c7b98f24836c5377b2eeedb9e623d883
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-30 01:34:22 -0300

    update index.js

--- scripts/Index/index.js
+++ scripts/Index/index.js
@@ -2,11 +2,13 @@
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "g");
+    var results = [];
+    var match;
+    while (match = regex.exec(url)) {
+        results.push(decodeURIComponent(match[2].replace(/\+/g, " ")));
+    }
+    return results;
 }
 
 const infos = document.querySelector('.item-description').innerHTML =   name

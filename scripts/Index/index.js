34ceb30e483f813fb77109a02510e0b386b6cbfc
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-30 00:50:30 -0300

    update index.js

--- scripts/Index/index.js
+++ scripts/Index/index.js
@@ -23,6 +23,3 @@ var picture = getParameterByName('picture');
 console.log("Email: " + email);
 console.log("Foto do Perfil: " + picture);
 
-setTimeout(function() {
-    location.reload();
-}, 1000);

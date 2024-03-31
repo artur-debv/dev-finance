a3536a5c64bf4b53ff543b0a7fa51705231d334a
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-31 16:36:25 -0300

    update index.html

--- scripts/Index/index.js
+++ scripts/Index/index.js
@@ -1,25 +1,32 @@
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "g");
-    var results = [];
-    var match;
-    while (match = regex.exec(url)) {
-        results.push(decodeURIComponent(match[2].replace(/\+/g, " ")));
+window.onload = function () {
+    // Recuperar o token JWT do localStorage
+    var jwtToken = localStorage.getItem('jwtToken');
+    if (jwtToken) {
+        // Decodificar o token JWT e fazer o que for necessário com as informações
+        var decodedToken = parseJwt(jwtToken);
+        console.log(decodedToken);
+        // Exemplo de como acessar as informações do token
+        var name = decodedToken.given_name;
+        var email = decodedToken.email;
+        var picture = decodedToken.picture;
+        // Faça o que for necessário com as informações
+
+        // Recuperar a URL completa da sessionStorage
+        var url = sessionStorage.getItem('url');
+        if (url) {
+            // Extrair as informações da query string da URL
+            var pictureParam = getParameterByName('picture', url);
+            console.log("Picture parameter from URL: " + pictureParam);
+            // Faça o que for necessário com as informações da query string
+        } else {
+            console.error("URL não encontrada na sessionStorage.");
+            // Lidar com o erro de forma apropriada
+        }
+    } else {
+        console.error("Token JWT não encontrado no localStorage.");
+        // Lidar com o erro de forma apropriada
     }
-    return results;
 }
-
-// Função para obter parâmetros de query string da sessionStorage
-function getParameterFromSessionStorage(name) {
-    var url = localStorage.getItem('url');
-    if (url) {
-        return getParameterByName(name, url);
-    }
-    return null;
-}
-
-
 
 
 const infos = document.querySelector('.item-description').innerHTML =   name

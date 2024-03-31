6ad30b5f72e37c80e28a547bd746a0593ee62a70
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-31 16:26:42 -0300

    update index.js

--- scripts/Index/index.js
+++ scripts/Index/index.js
@@ -1,15 +1,19 @@
-// Função para obter parâmetros de query string da URL
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "g");
-    var results = [];
-    var match;
-    while (match = regex.exec(url)) {
-        results.push(decodeURIComponent(match[2].replace(/\+/g, " ")));
+window.onload = function () {
+    // Recuperar o token JWT da sessionStorage
+    var jwtToken = sessionStorage.getItem('jwtToken');
+    if (jwtToken) {
+        // Decodificar o token JWT e fazer o que for necessário com as informações
+        var decodedToken = parseJwt(jwtToken);
+        console.log(decodedToken);
+        // Exemplo de como acessar as informações do token
+        var name = decodedToken.given_name;
+        var email = decodedToken.email;
+        var picture = decodedToken.picture;
+        // Faça o que for necessário com as informações
+    } else {
+        console.error("Token JWT não encontrado na sessionStorage.");
+        // Lidar com o erro de forma apropriada
     }
-    return results;
-    
 }
 
 // Obter o valor do parâmetro 'picture' da URL

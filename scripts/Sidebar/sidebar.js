8d4ef19f95de9b6e97186ff0c0d86205bbf81bdd
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-31 20:08:02 -0300

    update sidebar.js

--- scripts/Sidebar/sidebar.js
+++ scripts/Sidebar/sidebar.js
@@ -1,13 +1,3 @@
 document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-        // Redirecionar para a página de login ou fazer outra ação após o logout
-    });
-}
-
-document.getElementById("logout_btn").addEventListener("click", signOut);

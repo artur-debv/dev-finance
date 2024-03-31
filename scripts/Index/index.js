41d57fd1c53410d214a12fe92f36c0cb873f3ba9
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-31 15:57:28 -0300

    update index.js

--- scripts/Index/index.js
+++ scripts/Index/index.js
@@ -21,8 +21,16 @@ var imgElement = document.createElement('img');
 // Definir o atributo src do elemento <img> com a URL da imagem
 imgElement.src = pictureUrl;
 
-// Selecionar o elemento com o id 'user' no sidebar
-var userElement = document.getElementById('user');
+// Selecionar o elemento com o id 'user_avatar' no sidebar
+var userAvatarElement = document.getElementById('user_avatar');
+
+// Verificar se o elemento foi encontrado
+if (userAvatarElement) {
+    // Adicionar a imagem como filho do elemento 'user_avatar'
+    userAvatarElement.appendChild(imgElement);
+} else {
+    console.error("Elemento com id 'user_avatar' não encontrado.");
+}
 
 
 

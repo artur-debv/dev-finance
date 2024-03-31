37d3a08937eae8fefaf62f475ac5fdc6f4985438
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-31 16:44:49 -0300

    update index.js

--- scripts/Index/index.js
+++ scripts/Index/index.js
@@ -12,6 +12,14 @@ function getParameterByName(name, url) {
     
 }
 
+// Obter o valor do parâmetro 'picture' da URL
+var pictureUrl = getParameterByName('picture');
+
+// Selecionar o elemento com o id 'user_avatar'
+var avatarElement = document.getElementById('user_avatar');
+
+// Definir o atributo src do elemento <img> com a URL da imagem
+avatarElement.src = pictureUrl;
 
 
 const infos = document.querySelector('.item-description').innerHTML =   name
@@ -22,7 +30,7 @@ const infos = document.querySelector('.item-description').innerHTML =   name
 // Obter os valores dos parâmetros de query string
 var name = getParameterByName('name');
 var email = getParameterByName('email');
-var picture = getParameterByName('picture');
+
 
 // Usar os valores obtidos conforme necessário
 

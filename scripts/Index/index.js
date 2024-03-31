df3a93020d5b4950467da282766a17a9161f2f64
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-31 16:28:30 -0300

    update index.js

--- scripts/Index/index.js
+++ scripts/Index/index.js
@@ -1,3 +1,20 @@
+
+function parseJwt(token) {
+    try {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        return JSON.parse(jsonPayload);
+    } catch (error) {
+        console.error("Erro ao decodificar o token JWT:", error);
+        return null;
+    }
+}
+
+
 window.onload = function () {
     // Recuperar o token JWT da sessionStorage
     var jwtToken = sessionStorage.getItem('jwtToken');

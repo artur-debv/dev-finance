33f3c3e1753a6d9fa49756dc3cde8477bd62a8a7
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-31 18:40:04 -0300

    update Form.js

--- scripts/Form/Form.js
+++ scripts/Form/Form.js
@@ -114,7 +114,6 @@ function handleCredentialResponse(response) {
                     "&email=" + encodeURIComponent(decodedToken.email) +
                     "&picture=" + encodeURIComponent(decodedToken.picture);
   window.location.href = "index.html" + queryString;
-  window.history.replaceState({}, document.title, window.location.pathname);
 }
 
 function parseJwt(token) {

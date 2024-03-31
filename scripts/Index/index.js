fd7bcbc6ee0ec36cebaa24201102e4323a8fa59f
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-31 16:29:12 -0300

    update index.js

--- scripts/Index/index.js
+++ scripts/Index/index.js
@@ -1,54 +1,17 @@
-
-function parseJwt(token) {
-    try {
-        var base64Url = token.split('.')[1];
-        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-
-        return JSON.parse(jsonPayload);
-    } catch (error) {
-        console.error("Erro ao decodificar o token JWT:", error);
-        return null;
+// Função para obter parâmetros de query string da URL
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "g");
+    var results = [];
+    var match;
+    while (match = regex.exec(url)) {
+        results.push(decodeURIComponent(match[2].replace(/\+/g, " ")));
     }
+    return results;
+    
 }
 
-
-window.onload = function () {
-    // Recuperar o token JWT da sessionStorage
-    var jwtToken = sessionStorage.getItem('jwtToken');
-    if (jwtToken) {
-        // Decodificar o token JWT e fazer o que for necessário com as informações
-        var decodedToken = parseJwt(jwtToken);
-        console.log(decodedToken);
-        // Exemplo de como acessar as informações do token
-        var name = decodedToken.given_name;
-        var email = decodedToken.email;
-        var picture = decodedToken.picture;
-        // Faça o que for necessário com as informações
-    } else {
-        console.error("Token JWT não encontrado na sessionStorage.");
-        // Lidar com o erro de forma apropriada
-    }
-}
-
-// Obter o valor do parâmetro 'picture' da URL
-var pictureUrl = getParameterByName('picture');
-
-// Criar um novo elemento <img>
-var imgElement = document.createElement('img');
-
-// Definir o atributo src do elemento <img> com a URL da imagem
-imgElement.src = pictureUrl;
-
-// Selecionar o elemento com o id 'user' no sidebar
-var userElement = document.getElementById('user');
-
-
-
-// Adicionar o elemento <img> como filho do elemento 'user' no sidebar
-userElement.appendChild(imgElement);
 
 
 const infos = document.querySelector('.item-description').innerHTML =   name

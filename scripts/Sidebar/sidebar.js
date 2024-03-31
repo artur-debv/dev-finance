9b0e55b5f45e1a59439e2ae88e98be5f812b923f
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-31 19:48:15 -0300

    update sidebar.js

--- scripts/Sidebar/sidebar.js
+++ scripts/Sidebar/sidebar.js
@@ -2,9 +2,12 @@ document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
 
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
+document.addEventListener("DOMContentLoaded", function() {
+    // Seu código que depende da API do Google aqui
+
+    // Exemplo: definir um manipulador de eventos para o botão de logout
+    var logoutButton = document.getElementById("logoutButton");
+    logoutButton.addEventListener("click", function() {
+        signOut();
     });
-}
+});

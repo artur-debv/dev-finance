fb81df8a24fc5df70ab2691f18b652c303c45b76
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-31 20:34:20 -0300

    update sidebar.js

--- scripts/Sidebar/sidebar.js
+++ scripts/Sidebar/sidebar.js
@@ -2,14 +2,20 @@ document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
 
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-      // Faça outras ações após o logout, como redirecionar para a página de login
-    }).catch(function (error) {
-      console.error('Error signing out:', error);
+  // Função de inicialização após a biblioteca gapi ser carregada
+  function onGapiLoad() {
+    // Inicializar a biblioteca auth2
+    gapi.load('auth2', function() {
+        gapi.auth2.init({
+            client_id: '939125828914-u6tbs2k30r4tn6fr17k0erjb6j39l69d.apps.googleusercontent.com'
+        });
     });
 }
 
-signOut()
+// Função para desconectar a conta do Google
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}

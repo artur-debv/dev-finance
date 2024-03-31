33641cb839c051a22b34527bb3775386d45f1d0e
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-31 15:42:29 -0300

    update index.js

--- scripts/Index/index.js
+++ scripts/Index/index.js
@@ -12,7 +12,23 @@ function getParameterByName(name, url) {
     
 }
 
+// Obter o valor do parâmetro 'picture' da URL
+var pictureUrl = getParameterByName('picture');
 
+// Criar um novo elemento <img>
+var imgElement = document.createElement('img');
+
+// Definir o atributo src do elemento <img> com a URL da imagem
+imgElement.src = pictureUrl;
+
+// Selecionar o elemento com o id 'user' no sidebar
+var userElement = document.getElementById('user_avatar');
+
+// Limpar o conteúdo atual do elemento 'user' (caso necessário)
+userElement.innerHTML = '';
+
+// Adicionar o elemento <img> como filho do elemento 'user' no sidebar
+userElement.appendChild(imgElement);
 
 
 const infos = document.querySelector('.item-description').innerHTML =   name

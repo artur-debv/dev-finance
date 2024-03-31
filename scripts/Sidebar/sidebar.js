750e04128e54e60cc3ca7aaabcd9cca53f0d32e6
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-31 16:56:35 -0300

    update sidebar.js

--- scripts/Sidebar/sidebar.js
+++ scripts/Sidebar/sidebar.js
@@ -1,3 +1,12 @@
 document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
+
+function handleLogout() {
+    // Cancelar a autenticação com a conta do Google
+    google.accounts.id.cancel();
+
+}
+
+// Adicionar um evento de clique ao botão de logout
+document.getElementById('logout_btn').addEventListener('click', handleLogout);

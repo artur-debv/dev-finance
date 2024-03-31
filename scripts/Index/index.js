8ed682d68bbb9d260d010e56e69433cbcc554596
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-31 16:30:00 -0300

    update index.js

--- scripts/Index/index.js
+++ scripts/Index/index.js
@@ -1,3 +1,22 @@
+
+window.onload = function () {
+    // Recuperar o token JWT da sessionStorage
+    var jwtToken = sessionStorage.getItem('jwtToken');
+    if (jwtToken) {
+        // Decodificar o token JWT e fazer o que for necessário com as informações
+        var decodedToken = parseJwt(jwtToken);
+        console.log(decodedToken);
+        // Exemplo de como acessar as informações do token
+        var name = decodedToken.given_name;
+        var email = decodedToken.email;
+        var picture = decodedToken.picture;
+        // Faça o que for necessário com as informações
+    } else {
+        console.error("Token JWT não encontrado na sessionStorage.");
+        // Lidar com o erro de forma apropriada
+    }
+}
+
 // Função para obter parâmetros de query string da URL
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;

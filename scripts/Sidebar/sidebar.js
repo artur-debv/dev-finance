1eae0ee29afeec9712c94fe6a4955cba1faac8f2
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-03-31 21:05:03 -0300

    update sidebar.js

--- scripts/Sidebar/sidebar.js
+++ scripts/Sidebar/sidebar.js
@@ -2,11 +2,21 @@ document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
 
+import { loadAuth2, gapi } from 'gapi';
+
+// Carregar a biblioteca de autenticação do Google
+loadAuth2().then(() => {
+    console.log('Google Auth2 loaded.');
+}).catch((error) => {
+    console.error('Error loading Google Auth2:', error);
+});
+
+// Função para desconectar a conta do Google
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
+    auth2.signOut().then(() => {
+        console.log('User signed out.');
+    }).catch((error) => {
+        console.error('Error signing out:', error);
     });
-  }
-
-  signOut()
+}

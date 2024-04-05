3af76902b95436e0644ff07957cb92b6c9f5faee
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-04-05 11:22:36 -0300

    update InputSearch.js

---
+++ scripts/InputSearch/inputSearch.js
@@ -0,0 +1,17 @@
+document.querySelector('input_search').addEventListener('input', function() {
+    var searchText = this.value.toLowerCase(); // Obtém o valor digitado e converte para minúsculas para facilitar a comparação
+    var transactions = document.querySelectorAll('#tbodyID tr'); // Obtém todas as linhas de transação
+
+    transactions.forEach(function(transaction) {
+        var transactionName = transaction.querySelector('.transactionName').textContent.toLowerCase(); // Obtém o nome da transação
+        var transactionDescription = transaction.querySelector('.transactionDescription').textContent.toLowerCase(); // Obtém a descrição da transação
+        var transactionDate = transaction.querySelector('.transactionDate').textContent.toLowerCase(); // Obtém a data da transação
+
+        // Verifica se o texto da pesquisa está presente em qualquer parte do nome, descrição ou data da transação
+        if (transactionName.includes(searchText) || transactionDescription.includes(searchText) || transactionDate.includes(searchText)) {
+            transaction.style.display = ''; // Exibe a transação se houver uma correspondência
+        } else {
+            transaction.style.display = 'none'; // Oculta a transação se não houver correspondência
+        }
+    });
+});

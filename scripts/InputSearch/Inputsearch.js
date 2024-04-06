eb489b717d5bb371f613013e5652ed14335701d9
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-04-06 18:21:06 -0300

    update

--- scripts/InputSearch/Inputsearch.js
+++ scripts/InputSearch/Inputsearch.js
@@ -1,10 +1,22 @@
-const inputSearch = document.querySelector('.input_search');
-        const tableRows = document.querySelectorAll('#data-table tbody tr');
 
-        inputSearch.addEventListener('input', () => {
-            const searchTerm = inputSearch.value.toLowerCase();
-            tableRows.forEach(row => {
-                const description = row.querySelector('td:first-child').textContent.toLowerCase();
-                row.style.display = description.includes(searchTerm) ? 'table-row' : 'none';
-            });
-        });
+const Form = document.querySelector('.input_search').value
+
+console.log(Form)
+
+/*document.querySelector('input_search').addEventListener('keyup', function() {
+    var searchText = this.value.toLowerCase(); // Obtém o valor digitado e converte para minúsculas para facilitar a comparação
+    var transactions = document.querySelectorAll('#tbodyID tr'); // Obtém todas as linhas de transação
+
+    transactions.forEach(function(transaction) {
+        var transactionName = transaction.querySelector('.transactionName').textContent.toLowerCase(); // Obtém o nome da transação
+        var transactionDescription = transaction.querySelector('.transactionDescription').textContent.toLowerCase(); // Obtém a descrição da transação
+        var transactionDate = transaction.querySelector('.transactionDate').textContent.toLowerCase(); // Obtém a data da transação
+
+        // Verifica se o texto da pesquisa está presente em qualquer parte do nome, descrição ou data da transação
+        if (transactionName.includes(searchText) || transactionDescription.includes(searchText) || transactionDate.includes(searchText)) {
+            transaction.style.display = ''; // Exibe a transação se houver uma correspondência
+        } else {
+            transaction.style.display = 'none'; // Oculta a transação se não houver correspondência
+        }
+    });
+});*/

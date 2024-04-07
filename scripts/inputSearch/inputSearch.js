d8947d87d4e7f36d10a3477698abdcadc48bf530
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-04-07 01:06:46 -0300

    update

--- scripts/inputSearch/inputSearch.js
+++ scripts/inputSearch/inputSearch.js
@@ -4,7 +4,7 @@ const buttons = document.getElementById('button_search')
 
 
 buttons.addEventListener('click',function(){
-    const inputValue = document.getElementById('input_Search').value;
+    const inputValue = document.getElementById('input_Search').value.toLowerCase()
     console.log(inputValue);
 })
 

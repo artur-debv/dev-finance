8d4a11e29cf30e30854dc405151c23d25cd54a74
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-04-07 00:23:21 -0300

    update inputSearch.js

--- scripts/inputSearch/inputSearch.js
+++ scripts/inputSearch/inputSearch.js
@@ -1,7 +1,7 @@
 
 const buttons = document.getElementById('button_search')
 const input = document.getElementById('input_search')
-console.log(inputs)
+console.log(input)
 
 buttons.addEventListener('click',function(){
     

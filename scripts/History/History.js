64f560a3c2fd4d0a3c4e6d74dd35585bd29e68c8
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-04-09 10:51:57 -0300

    update

--- scripts/History/History.js
+++ scripts/History/History.js
@@ -8,12 +8,10 @@ document.addEventListener('DOMContentLoaded', () => {
            mode.classList.remove('fa-moon');
            mode.classList.add('fa-sun');
            form.classList.add('dark');
-           form2.classList.add('select');
        } else {
            mode.classList.remove('fa-sun');
            mode.classList.add('fa-moon');
            form.classList.remove('dark');
-           form2.classList.remove('select');
        }
    }
 

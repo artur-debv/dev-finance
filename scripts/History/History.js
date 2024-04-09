6a8768c4ddea5e841d71d0bba9840a911ad5883f
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-04-09 10:59:04 -0300

    update

--- scripts/History/History.js
+++ scripts/History/History.js
@@ -1,39 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-   const mode = document.getElementById('mode_icon');
-   const form = document.querySelector('.darkmode');
 
+ const Historys = document.querySelector(".a_history")
 
-   function setTheme(theme) {
-       if (theme === 'dark') {
-           mode.classList.remove('fa-moon');
-           mode.classList.add('fa-sun');
-           form.classList.add('dark');
-       } else {
-           mode.classList.remove('fa-sun');
-           mode.classList.add('fa-moon');
-           form.classList.remove('dark');
-       }
-   }
-
-   function toggleTheme() {
-       const currentTheme = localStorage.getItem('theme') || 'light';
-       const newTheme = currentTheme === 'light' ? 'dark' : 'light';
-       localStorage.setItem('theme', newTheme);
-       setTheme(newTheme);
-   }
-
-   function setThemeFromPreference() {
-       const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
-       const systemPreference = prefersDarkScheme.matches ? 'dark' : 'light';
-       if (!localStorage.getItem('theme')) {
-           localStorage.setItem('theme', systemPreference);
-       }
-       setTheme(localStorage.getItem('theme'));
-   }
-
-   setThemeFromPreference();
-
-   mode.addEventListener('click', () => {
-       toggleTheme();
-   });
-});
+ Historys.addEventListener("click",function(){
+    window.open("nova_pagina.html", "_self")
+ })
+ 

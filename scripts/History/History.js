6759dbb8df95b80ccb55316acf54d308022d8be8
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-04-10 10:59:09 -0300

    update

--- scripts/History/History.js
+++ scripts/History/History.js
@@ -26,8 +26,8 @@ monthlyExpensesArray.sort((a, b) => b[1] - a[1]);
 const topMonths = monthlyExpensesArray.slice(0, 3); // Pegar os 3 primeiros meses mais gastos
 
 
-const Date = document.querySelector(".Data").innerHTML = monthlyExpenses[0][0]
-const Value = document.querySelector(".Valor").innerHTML = monthlyExpenses[1][1]
+const Date = document.querySelector(".Data")
+const Value = document.querySelector(".Valor")
 
 
 console.log('Meses mais gastos:', topMonths);

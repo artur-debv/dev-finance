a4d66ffac54f9640686b5cfc7456fafb7a593752
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-04-09 21:11:02 -0300

    update History.js

--- scripts/History/History.js
+++ scripts/History/History.js
@@ -5,3 +5,23 @@
     window.open("nova_pagina.html", "_self")
  })
  
+
+ const transactionDescription = transaction.querySelector('td:nth-child(1)')
+ const transactionValue = transaction.querySelector('td:nth-child(2)')
+ const transactionDate = transaction.querySelector('td:nth-child(3)')
+
+
+ const transaction = {
+   transactionDescription: descricao,
+   transactionValue: valor,
+   transactionDate: data
+ };
+
+ // Obtendo o array de transações armazenado no localStorage
+ const transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+ 
+ // Adicionando a nova transação ao array
+ transactions.push(transaction);
+
+ // Salvando o array atualizado de transações no localStorage
+ localStorage.setItem('transactions', JSON.stringify(transactions));

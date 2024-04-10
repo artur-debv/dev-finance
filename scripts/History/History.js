6cc4f01f6ca37e1295638dcf16fc1a996a4fb9f2
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-04-09 23:07:38 -0300

    update

--- scripts/History/History.js
+++ scripts/History/History.js
@@ -3,22 +3,24 @@ const transactions = JSON.parse(localStorage.getItem("dev.finances:transactions"
 
 // Objeto para armazenar o total gasto em cada mês
 const monthlyExpenses = {};
-console.log(monthlyExpenses)
 
 // Percorrendo as transações
 transactions.forEach(transaction => {
+    // Dividindo a transação em nome, valor e data
+    const [description, amount, date] = transaction.split(' - ');
+
     // Extraindo o mês e o ano da data da transação
-    const [day, month, year] = transaction.date.split('/'); // Aqui estamos dividindo a data em dia, mês e ano
-    const monthYear = `${day}/${month}/${year}`;
+    const [day, month, year] = date.split('/'); // Aqui estamos dividindo a data em dia, mês e ano
+    const monthYear = `${month}/${year}`;
 
     // Removendo caracteres não numéricos do valor da transação e convertendo para um número
-    const amount = parseFloat(transaction.amount.replace(/[^\d,-]/g, '').replace(',', '.')); // Remove caracteres não numéricos e substitui vírgula por ponto
+    const amountNumber = parseFloat(amount.replace(/[^\d,-]/g, '').replace(',', '.')); // Remove caracteres não numéricos e substitui vírgula por ponto
 
     // Se o mês já estiver no objeto monthlyExpenses, adiciona o valor da transação à soma existente; caso contrário, cria uma nova entrada com o valor da transação
     if (monthlyExpenses[monthYear]) {
-        monthlyExpenses[monthYear] += amount;
+        monthlyExpenses[monthYear] += amountNumber;
     } else {
-        monthlyExpenses[monthYear] = amount;
+        monthlyExpenses[monthYear] = amountNumber;
     }
 });
 

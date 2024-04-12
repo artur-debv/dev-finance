56109cccc6191d6142aaaeadb9d61917a51378d0
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-04-11 22:09:09 -0300

    update

--- scripts/Sidebar/sidebar.js
+++ scripts/Sidebar/sidebar.js
@@ -1,3 +1,12 @@
 document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+        // Redirecione para a página de login ou outra página após o logout
+        window.location.href = "https://devfinancesss.netlify.app/form";
+    });
+}

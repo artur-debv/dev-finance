7ffcaa439f670e1d22f9a2c4845cd7361de61b68
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-04-12 11:29:58 -0300

    update

--- scripts/Sidebar/sidebar.js
+++ scripts/Sidebar/sidebar.js
@@ -2,11 +2,9 @@ document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
 
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-        // Redirecione para a página de login ou outra página após o logout
-        window.location.href = "https://devfinancesss.netlify.app/form";
-    });
+ // Função para desconectar o usuário da conta do Google
+ function signOut() {
+    google.accounts.id.disableAutoSelect();
+    // Redirecione para a página de login ou faça outras ações de logout, se necessário
+    window.location.href = "https://seusite.com/form.html"; // Altere para o URL correto da página de login
 }

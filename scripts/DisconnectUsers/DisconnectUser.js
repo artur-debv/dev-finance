e3aba1d2cc466eaf5a8bb0801472b155caa26dfb
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-06-18 11:01:31 -0300

    feat: I added the functionality to remove the token from localStorage

--- scripts/DisconnectUsers/DisconnectUser.js
+++ scripts/DisconnectUsers/DisconnectUser.js
@@ -1,12 +1,13 @@
 function disconnectUser() {
+    localStorage.removeItem('jwtToken');
     // Verifica se a biblioteca está carregada
     if (typeof google.accounts !== 'undefined' && google.accounts.id) {
         try {
-            
-             google.accounts.id.revoke();
+
+            google.accounts.id.revoke();
 
             // Redireciona para a página desejada após a desconexão (opcional)
-           window.location.href = "https://dev-finance-seven-inky.vercel.app/Form.html";
+            window.location.href = "https://dev-finance-seven-inky.vercel.app/Form.html";
         } catch (error) {
             console.error('Erro ao revogar o token de autenticação:', error);
             // Trate o erro aqui, se necessário

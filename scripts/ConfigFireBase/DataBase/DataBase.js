fe839e602e8e57345d3a3d65d84b5357e7e1e1c6
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-06-20 09:49:45 -0300

    updates

---
+++ scripts/ConfigFireBase/DataBase/DataBase.js
@@ -0,0 +1,6 @@
+const Description = document.querySelector("td .description");
+console.log(Description)
+const Amount = document.querySelector("td .income");
+console.log(Amount)
+const Date = document.querySelector("td .date");
+console.log(Date)

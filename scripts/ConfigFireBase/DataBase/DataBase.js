dce069ec6e9bad60cef1d030334413f0a479f4e6
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-06-20 11:01:54 -0300

    update index.html and DataBase.js

--- scripts/ConfigFireBase/DataBase/DataBase.js
+++ scripts/ConfigFireBase/DataBase/DataBase.js
@@ -2,6 +2,8 @@ const Description = document.querySelector("input#description")
 const Amount = document.querySelector("input#amount")
 const Date = document.querySelector("input#date")
 
+
+
 const Db = firebase.database()
 
 console.log(Db)

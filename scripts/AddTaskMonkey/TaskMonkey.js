059b5b003861883a0cd05ecf0c6985b859c98a64
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-06-25 10:31:28 -0300

    updates

--- scripts/AddTaskMonkey/TaskMonkey.js
+++
@@ -1,8 +0,0 @@
-fetch("https://api.sheetmonkey.io/form/jxtYmjVnUoWL7zCNeE7XjQ", {
-    method: "POST",
-}).then(() => {
-    const successMessage = document.createElement("h1");
-    successMessage.textContent = "Tarefas adicionadas com sucesso!";
-    successMessage.style.color = "green"; // Estilize conforme necessário
-    document.body.appendChild(successMessage);
-});

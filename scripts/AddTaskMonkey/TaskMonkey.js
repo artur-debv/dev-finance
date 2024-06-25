ee2ce5e69623e74550fcc71564b9f7165eade01f
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-06-25 10:46:33 -0300

    refactor: I added styling at the time of the response to make it more intuitive

--- scripts/AddTaskMonkey/TaskMonkey.js
+++ scripts/AddTaskMonkey/TaskMonkey.js
@@ -1,9 +1,30 @@
 
 const button = document.querySelector(".button_submit");
 
-button.addEventListener("click", (event) => {
+
+const HandleSubmit = (event) => {
+    event.preventDefault();
+
+    const description = document.querySelector("input#description").value;
+    const amount = document.querySelector("input#amount").value;
+    const date = document.querySelector("input#date").value;
+
     fetch("https://api.sheetmonkey.io/form/jxtYmjVnUoWL7zCNeE7XjQ", {
         method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "Description": description,
+            "Amount": amount,
+            "Date": date
+        })
+
     })
-})
+}
+
+document.querySelector("form").addEventListener("submit", HandleSubmit);
+
+
 

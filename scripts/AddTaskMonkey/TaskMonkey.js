cff239bb9b16ac2419ecf8a5b09b73fe331130be
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-06-25 10:11:32 -0300

    refactor: I added styling at the time of the response to make it more intuitive

--- scripts/AddTaskMonkey/TaskMonkey.js
+++ scripts/AddTaskMonkey/TaskMonkey.js
@@ -1,8 +1,5 @@
 fetch("https://api.sheetmonkey.io/form/jxtYmjVnUoWL7zCNeE7XjQ", {
     method: "POST",
 }).then(() => {
-    const successMessage = document.createElement("div");
-    successMessage.textContent = "Tarefas adicionadas com sucesso!";
-    successMessage.style.color = "green"; 
-    document.body.appendChild(successMessage);
+   alert("Enviado");
 });

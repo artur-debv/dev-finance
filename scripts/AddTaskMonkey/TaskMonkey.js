7bde5c615d1f0f35433b4f6ad60c36c4e5084c4f
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-06-26 08:58:36 -0300

    refactor: I removed a script that was no longer being used in the code

--- scripts/AddTaskMonkey/TaskMonkey.js
+++ scripts/AddTaskMonkey/TaskMonkey.js
@@ -1,26 +1,9 @@
-const HandleSubmit = () => {
-
-  event.preventDefault();
-
-  const description = document.querySelector("input#description").value;
-  const amount = document.querySelector("input#amount").value;
-  const date = document.querySelector("input#date").value;
-
-  fetch('https://api.sheetmonkey.io/form/jxtYmjVnUoWL7zCNeE7XjQ', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({description, amount, date})
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('Success:', data);
+fetch("https://api.sheetmonkey.io/form/jxtYmjVnUoWL7zCNeE7XjQ")
+.then((response) => {
+    response.json().then((data) => {
+        console.log(data)
     })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-}
-
-document.querySelector("form").addEventListener("click", HandleSubmit)
+.catch((error) => {
+    console.log(error)
+})
+})

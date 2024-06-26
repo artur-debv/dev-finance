900daa3c1f42f33dc929a6258cb5c5f7972900b0
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-06-26 08:50:41 -0300

    feat: I added a new functionality that can be added to the transaction in Excel

--- scripts/AddTaskMonkey/TaskMonkey.js
+++ scripts/AddTaskMonkey/TaskMonkey.js
@@ -1,30 +1,26 @@
-const handleSubmit = (event) => {
-    event.preventDefault();
+const HandleSubmit = () => {
 
-    const description = document.querySelector("input#description").value;
-    const amount = document.querySelector("input#amount").value;
-    const date = document.querySelector("input#date").value;
+  event.preventDefault();
 
-    fetch("http://localhost:3000/api/form/jxtYmjVnUoWL7zCNeE7XjQ", {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-        },
-        body: JSON.stringify({ description, amount, date }),
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`Network response was not ok: ${response.statusText}`);
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Success:', data);
+  const description = document.querySelector("input#description").value;
+  const amount = document.querySelector("input#amount").value;
+  const date = document.querySelector("input#date").value;
+
+  fetch('https://api.sheetmonkey.io/form/jxtYmjVnUoWL7zCNeE7XjQ', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({description, amount, date})
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data);
     })
     .catch((error) => {
-        console.error('Error:', error);
+      console.error('Error:', error);
     });
-};
+}
 
-document.querySelector("form").addEventListener("submit", handleSubmit);
+document.querySelector("form").addEventListener("submit", HandleSubmit)

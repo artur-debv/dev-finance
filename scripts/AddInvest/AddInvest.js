dcfde1ef087ca4ad5fbb73dc588017ec580ced67
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-07-12 22:16:32 -0300

    updates

--- scripts/AddInvest/AddInvest.js
+++ scripts/AddInvest/AddInvest.js
@@ -12,11 +12,11 @@ const Modal = {
   const Storage = {
     // Objeto responsável por lidar com o armazenamento de transações no localStorage
     get() {
-      return JSON.parse(localStorage.getItem("dev.finances:investments")) || []; // Obtém as transações armazenadas no localStorage ou retorna um array vazio se não houver transações
+      return JSON.parse(localStorage.getItem("dev.financesss:investments")) || []; // Obtém as transações armazenadas no localStorage ou retorna um array vazio se não houver transações
     },
     set(transactions) {
       localStorage.setItem(
-        "dev.finances:investments",
+        "dev.financesss:investments",
         JSON.stringify(transactions)
       ); // Define as transações no localStorage após converter para JSON
     },

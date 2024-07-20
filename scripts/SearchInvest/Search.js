993c7ea77d4cf224f4a4235744ce8bf028125432
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-07-20 19:39:36 -0300

    updates

--- scripts/SearchInvest/Search.js
+++ scripts/SearchInvest/Search.js
@@ -1,23 +1,22 @@
 function search() {
     const buttonSearch = document.getElementById('button_search');
-
+  
     buttonSearch.addEventListener('click', function () {
-        const selectedInvestmentType = document.getElementById('tipoInvestimento').value.toLowerCase();
-        const transactions = document.querySelectorAll('.tbodys tr');
-
-        transactions.forEach(function (transaction) {
-            const transactionInvestmentType = transaction.querySelector('td:nth-child(1)').textContent.toLowerCase();
-            // Você pode adicionar mais filtros, se necessário
-
-            const matchesInvestmentType = selectedInvestmentType === "" || transactionInvestmentType.includes(selectedInvestmentType);
-
-            if (matchesInvestmentType) {
-                transaction.style.display = ''; // Exibe a transação se houver correspondência
-            } else {
-                transaction.style.display = 'none'; // Oculta a transação se não houver correspondência
-            }
-        });
+      const selectedInvestmentType = document.getElementById('tipoInvestimento').value.toLowerCase();
+      const transactions = document.querySelectorAll('.tbodys tr');
+  
+      transactions.forEach(function (transaction) {
+        const transactionInvestmentType = transaction.querySelector('td:nth-child(1)').textContent.toLowerCase();
+  
+        const matchesInvestmentType = selectedInvestmentType === "" || transactionInvestmentType.includes(selectedInvestmentType);
+  
+        if (matchesInvestmentType) {
+          transaction.style.display = ''; // Exibe a transação se houver correspondência
+        } else {
+          transaction.style.display = 'none'; // Oculta a transação se não houver correspondência
+        }
+      });
     });
-}
-
-document.addEventListener('DOMContentLoaded', search);
+  }
+  
+  document.addEventListener('DOMContentLoaded', search);

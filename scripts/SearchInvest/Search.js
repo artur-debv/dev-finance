947ed23c1f4f7be39cad7395b1bd68c0bfd23df9
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-07-20 21:15:23 -0300

    updates

--- scripts/SearchInvest/Search.js
+++ scripts/SearchInvest/Search.js
@@ -1,22 +1,27 @@
-function search() {
-    const buttonSearch = document.getElementById('button_search');
-  
-    buttonSearch.addEventListener('click', function () {
-      const selectedInvestmentType = document.getElementById('tipoInvestimento').value.toLowerCase();
-      const transactions = document.querySelectorAll('.tbodys tr');
-  
-      transactions.forEach(function (transaction) {
-        const transactionInvestmentType = transaction.querySelector('td:nth-child(1)').textContent.toLowerCase();
-  
-        const matchesInvestmentType = selectedInvestmentType === "" || transactionInvestmentType.includes(selectedInvestmentType);
-  
-        if (matchesInvestmentType) {
-          transaction.style.display = ''; // Exibe a transação se houver correspondência
-        } else {
-          transaction.style.display = 'none'; // Oculta a transação se não houver correspondência
-        }
-      });
-    });
-  }
-  
-  document.addEventListener('DOMContentLoaded', search);
+document.addEventListener('DOMContentLoaded', function () {
+    function search() {
+        const buttonSearch = document.getElementById('button_search');
+
+        buttonSearch.addEventListener('click', function () {
+            const inputSearch = document.getElementById('input_Search').value.toLowerCase();
+            console.log("Valor da busca:", inputSearch); // Log para debug
+
+            const transactions = document.querySelectorAll('.tbodys tr');
+            transactions.forEach(function (transaction) {
+                const transactionInvestmentType = transaction.querySelector('td:nth-child(1)').textContent.toLowerCase();
+                console.log("Tipo de investimento na transação:", transactionInvestmentType); // Log para debug
+
+                const matchesInvestmentType = inputSearch === "" || transactionInvestmentType.includes(inputSearch);
+                console.log("Corresponde ao tipo de investimento?", matchesInvestmentType); // Log para debug
+
+                if (matchesInvestmentType) {
+                    transaction.style.display = ''; // Exibe a transação se houver correspondência
+                } else {
+                    transaction.style.display = 'none'; // Oculta a transação se não houver correspondência
+                }
+            });
+        });
+    }
+
+    search(); // Chama a função search para adicionar o listener
+});

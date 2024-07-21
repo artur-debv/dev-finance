aa57f0fdf798f426846656f27d36d635973d88e5
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-07-20 21:19:19 -0300

    updates

--- scripts/SearchInvest/Search.js
+++ scripts/SearchInvest/Search.js
@@ -4,15 +4,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
         buttonSearch.addEventListener('click', function () {
             const inputSearch = document.getElementById('input_Search').value.toLowerCase();
-            console.log("Valor da busca:", inputSearch); // Log para debug
-
+            
             const transactions = document.querySelectorAll('.tbodys tr');
             transactions.forEach(function (transaction) {
                 const transactionInvestmentType = transaction.querySelector('td:nth-child(1)').textContent.toLowerCase();
                 console.log("Tipo de investimento na transação:", transactionInvestmentType); // Log para debug
 
                 const matchesInvestmentType = inputSearch === "" || transactionInvestmentType.includes(inputSearch);
-                console.log("Corresponde ao tipo de investimento?", matchesInvestmentType); // Log para debug
+              
 
                 if (matchesInvestmentType) {
                     transaction.style.display = ''; // Exibe a transação se houver correspondência

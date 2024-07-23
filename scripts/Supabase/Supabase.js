f63ce37d846cc5ea322c7bc99f49ae0005b7a990
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-07-23 10:04:38 -0300

    feat: I added the supabase database script, created the client for the database

--- scripts/Supabase/Supabase.js
+++ scripts/Supabase/Supabase.js
@@ -1,4 +1,4 @@
-const { createClient } = supabase;
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://your-project.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2bm92YnViZWFqb2t6aWduYWJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE3Mzc0MzcsImV4cCI6MjAzNzMxMzQzN30.zv5JXOAoTR6ivRcycWx6eOtV7uLLqIuxjQZX9jjMwvM';

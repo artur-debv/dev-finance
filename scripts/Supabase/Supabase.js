03bf97ab555f2214c3bf225f77958c585771bd04
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-07-23 11:13:17 -0300

    updates

--- scripts/Supabase/Supabase.js
+++
@@ -1,8 +0,0 @@
-// dist/supabaseClient.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const supabaseUrl = 'https://your-project.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2bm92YnViZWFqb2t6aWduYWJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE3Mzc0MzcsImV4cCI6MjAzNzMxMzQzN30.zv5JXOAoTR6ivRcycWx6eOtV7uLLqIuxjQZX9jjMwvM';
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;

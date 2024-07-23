ea306a9bdd07dc65c14e21a2032646820e22175f
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-07-23 10:09:51 -0300

    updates

--- scripts/Supabase/Supabase.js
+++ scripts/Supabase/Supabase.js
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://your-project.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2bm92YnViZWFqb2t6aWduYWJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE3Mzc0MzcsImV4cCI6MjAzNzMxMzQzN30.zv5JXOAoTR6ivRcycWx6eOtV7uLLqIuxjQZX9jjMwvM';
-const supabase = createClient(supabaseUrl, supabaseKey);
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://xyzcompany.supabase.co', 'public-anon-key')
